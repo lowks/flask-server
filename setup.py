30e61c04567222fe0e98e67e105768ab120f3792
Ondrej Sika <sika.ondrej@gmail.com>
2013-03-21 20:37:02 +0100

    add flask server init script (v.1.1)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "flask-server",
-    version = "1.0",
+    version = "1.1",
     url = 'http://ondrejsika.com/docs/flask-server',
     download_url = 'https://github.com/sikaondrej/flask-server',
     license = 'GNU LGPL v.3',
@@ -14,6 +14,7 @@ setup(
     author_email = 'dev@ondrejsika.com',
     packages = find_packages(),
     install_requires = ["flask", ],
+    scripts = ["flask_server/bin/flask-server", ],
     include_package_data = True,
     zip_safe = False,
 )
